53f3d76c88561cdd3b8ef6c45ae750a19cef0261
“Sofia <corvalansofia1@gmail.com>
2023-06-16 17:04:00 -0300

    bd

--- Data/SaturnoV2Context.cs
+++ Data/SaturnoV2Context.cs
@@ -30,7 +30,7 @@ public partial class SaturnoV2Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;Database=Saturno_V2;Trusted_connection=true;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=Saturno_V2;Trusted_connection=true;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
